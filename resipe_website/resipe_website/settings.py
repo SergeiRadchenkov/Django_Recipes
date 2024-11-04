e68d96bb9c6c5916e9b3999329755de9bace893e
SergeiRadchenkov <radchenkovsa@yandex.ru>
2024-11-04 21:13:33 +0300

    Заливаем на сервер

--- resipe_website/resipe_website/settings.py
+++ resipe_website/resipe_website/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x^^!zd@eomo!)^zyad!8hv_9pmsk&w-3n@tolbzx*fbe%_afmk'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
+    'sarancha.pythonanywhere.com',
 ]
 
 
@@ -78,8 +81,15 @@ WSGI_APPLICATION = 'resipe_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sarancha$default',
+        'USER': 'sarancha',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'sarancha.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -119,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = 'media/'
