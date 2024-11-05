1a284efc106a111d863cd267526500f105519313
SergeiRadchenkov <radchenkovsa@yandex.ru>
2024-11-06 00:18:58 +0300

    Завершение проекта

--- resipe_website/resipe_website/settings.py
+++ resipe_website/resipe_website/settings.py
@@ -21,15 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-x^^!zd@eomo!)^zyad!8hv_9pmsk&w-3n@tolbzx*fbe%_afmk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+DEBUG = True
+#SESSION_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = True
+
 
 ALLOWED_HOSTS = [
-    'sarancha.pythonanywhere.com',
+    'localhost',
+    '127.0.0.1',
 ]
 
 
@@ -81,15 +83,15 @@ WSGI_APPLICATION = 'resipe_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sarancha$default',
-        'USER': 'sarancha',
-        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': 'sarancha.mysql.pythonanywhere-services.com',
-        'OPTIONS': {
-            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        #'USER': 'sarancha',
+        #'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        #'HOST': 'sarancha.mysql.pythonanywhere-services.com',
+        #'OPTIONS': {
+        #    'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+        #    'charset': 'utf8mb4',
+        #},
     }
 }
 
@@ -129,8 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = 'media/'
 
